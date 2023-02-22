d5077ec1312906ba00326107d3aaf7034b22101c
Muhammad Haggy <muhammad.haggy@ui.ac.id>
2023-02-22 21:04:02 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+var allowlist = ['http://localhost:3000/', 'http://example2.com']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(corsOptionsDelegate)
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
